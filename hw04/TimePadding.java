801502e6c3da81be5d9e13b4bfcee0f86ae9b5a5
akr218 <akr218@lehigh.edu>
2014-09-24 00:21:36 +0000

    updated TimePadding

--- hw04/TimePadding.java
+++ hw04/TimePadding.java
@@ -22,16 +22,30 @@ public class TimePadding {
             int TimeSec = 0;
             TimeSec = myScanner.nextInt();
         
-        double TimeHr = TimeSec/3600;
-        double TimeMin = 0;
-        TimeHr%=TimeMin;
-        double TimeMin1 = TimeMin*60;
-        double TimeSec1 = 0;
-        TimeMin1%=TimeSec1;
-        double TimeSec2 = TimeSec1*60;
+        int TimeHr = TimeSec/3600;
+        int RemainingSeconds = TimeSec % 3600;
+        int TimeMin = RemainingSeconds/60;
+        int RemainingSeconds1 = RemainingSeconds % 60; 
         
-        System.out.println("The time in seconds is "+(int) TimeHr+":"+(int)TimeMin1+":"+(int)TimeSec2);
-        
+        if (TimeHr<10 && TimeMin<10 && RemainingSeconds1<10) {
+            System.out.println("The time in seconds is 0"+TimeHr+":0"+TimeMin+":0"+RemainingSeconds1+".");
+        }
+        else if(TimeHr<10 && TimeMin>10 && RemainingSeconds1<10) {
+            System.out.println("The time in seconds is 0"+TimeHr+":"+TimeMin+":0"+RemainingSeconds1+".");
+        }
+        else if(TimeHr<10 && TimeMin<10 && RemainingSeconds1>10) {
+            System.out.println("The time in seconds is 0"+TimeHr+":"+TimeMin+":"+RemainingSeconds1+".");
+        }
+        else if(TimeHr>10 && TimeMin>10 && RemainingSeconds1<10) {
+            System.out.println("The time in seconds is "+TimeHr+":"+TimeMin+":0"+RemainingSeconds1+".");
+        }
+        else if(TimeHr>10 && TimeMin>10 && RemainingSeconds1>10) {
+            System.out.println("The time in seconds is "+TimeHr+":"+TimeMin+":"+RemainingSeconds1+".");
+        }
+        else if(TimeHr<10 && TimeMin>10 && RemainingSeconds1>10) {
+            System.out.println("The time in seconds is 0"+TimeHr+":"+TimeMin+":"+RemainingSeconds1+".");
+        }
+      
         }//end of main method
     
     }//end of class
---
+++ lab04/BigMacAgain.java
@@ -0,0 +1,26 @@
+//Amanda Ritter
+//lab04
+//Big Mac Again Java Program 
+
+//This program computes the cost of a meal depending on the amount of Big Macs and fries provided
+//First compile the program
+    //javac BigMacAgain.java
+//Then run the program
+    //java BigMacAgain
+    
+import java.util.scanner
+
+public class BigMacAgain{
+    //main method required for every program 
+    public static void main(String[] args) {
+    
+    //Declare the scanner object
+    Scanner myScanner;
+    myScanner = new Scanner(System.in);
+    
+    System.out.print(
+        "Enter the number of Big Macs: ");
+        double nBigMacs = 0;
+        nBigMacs = myScanner.nextDouble();
+        
+    }
